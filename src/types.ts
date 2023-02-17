46d0c6f281ed2f65de65d1204e049951ddc2d386
trinhtthao203 <trinhtthao203@gmail.com>
2023-02-17 18:20:06 +0700

    alo

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type Book = {
+  id?: string;
+  title?: string;
+  author?: string[];
+  publisher?: string;
+  pushlished?: string;
+  imageLink?: string; //thumnail
+  bookLink?: string; //infoLink
+};
