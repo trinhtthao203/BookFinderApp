1b1423cd5ace209f6c27ae805c592f0f275a6a31
bangtuyenlam <bangtuyenlam@gmail.com>
2023-02-19 12:22:20 +0700

    if-else with image

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,46 +1,6 @@
-<!-- 
-<script lang="ts" setup>
-import type { BookList } from "../types";
-
-type PropTypes = {
-  book: BookList;
-};
-
-defineProps<PropTypes>();
-</script>
-
-<template>
-  <div>
-    <h4> <b>{{ book.volumeInfo.title }}</b> </h4>
-    <div>
-    <div v-if="book.volumeInfo.imageLinks?.smallThumbnail">
-        <img :src="book.volumeInfo.imageLinks?.smallThumbnail"  alt="img_book"/>
-
-    </div>
-    <div v-else>
-        <img width="100" height="100" src= "https:upload.wikimedia.org/wikipedia/commons/thumb/7/72/Placeholder_book.svg/2000px-Placeholder_book.svg.png"  alt="img_book"/>
-    </div>
-    </div>
-    
-
-    <p><b>Author: </b> <ul v-for="author in book.volumeInfo.authors" :key="author"><li>{{ author }}</li></ul></p>
-    <p><b>Publisher: </b> {{ book.volumeInfo.publisher }}</p>
-    <p><b>Published: </b> {{ book.volumeInfo.publishedDate }}</p>
-  
-
-    </div>
-</template>
-
-<style scoped>
-.n-card {
-  max-width: 50px;
-}
-</style> -->
-
 <script setup lang="ts">
 import type { BookList } from "../types";
 import { NCard, NA } from "naive-ui";
-
 type PropTypes = {
   book: BookList;
 };
@@ -51,10 +11,18 @@ defineProps<PropTypes>();
   <n-a :href="book.volumeInfo?.infoLink" id="container">
     <n-card :title="book.volumeInfo?.title" hoverable>
       <div style="display: flex; flex-direction: row; align-items: center">
+        <div v-if="book.volumeInfo?.imageLinks?.smallThumbnail">
         <img
           style="width: 120px"
           :src="book.volumeInfo?.imageLinks?.thumbnail"
         />
+      </div>
+      <div v-else>
+        <img
+          style="width: 120px"
+          src= "https:upload.wikimedia.org/wikipedia/commons/thumb/7/72/Placeholder_book.svg/2000px-Placeholder_book.svg.png"
+        />
+      </div>
         <div style="width: 70%">
           <p>
             <span style="font-weight: bold">Author:</span>
